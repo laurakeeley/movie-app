b0afceb5ecae2ed63c12ed278f8bccf76814948e
laura <l.keeley000@gmail.com>
2021-08-31 09:48:07 -0500

    adds restful routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,35 @@
 class MoviesController < ApplicationController
+  def index
+    render json: Movie.all
+  end
 
-def all_movies
-  movie = Movie.all
-  render json: movie
-end
+  def create
+    movie = Movie.new(
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot],
+    )
+    movie.save
+    render json: movie
+  end
 
-def movie
-  movie = Movie.find(params[:id])
-  render json: movie
-end
+  def show
+    movie = Movie.find(params[:id])
+    render json: movie
+  end
 
+  def update
+    movie = Movie.find(params[:id])
+    movie.title = params[:title] || movie.title
+    movie.year = params[:year] || movie.year
+    movie.plot = params[:plot] || movie.plot
+    movie.save
+    render json: movie
+  end
+
+  def destroy
+    movie = Movie.find(params[:id])
+    movie.destroy
+    render json: {message: "Movie has been successfully destroyed."}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get "/actor", controller: "actors", action: "actor"
-  get "/all_movies", controller: "movies", action: "all_movies"
-  get "/movie/:id", controller: "movies", action: "movie"
-  get "/actor/:id", controller: "actors", action: "actor_by_id"
-  post "/actor/:id", controller: "actors", action: "actor_by_id"
+  get "/actors" => "actors#index"
+  post "/actors" => "actors#create"
+  get "/actors/:id" => "actors#show"
+  patch "/actors/:id" => "actors#update"
+  delete "actors/:id" => "actors#destroy"
+  get "/movies" => "movies#index"
+  post "/movies" => "movies#create"
+  get "/movies/:id" => "movies#show"
+  patch "/movies/:id" => "movies#update"
+  delete "/movies/:id" => "movies#destroy"
 end
